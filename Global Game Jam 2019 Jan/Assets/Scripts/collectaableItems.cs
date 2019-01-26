b753675a6f874aac86e53cb66ccded2daf40622e
Kayleigh Bezaire <kayleigh.bezaire@gmail.com>
2019-01-26 11:31:16 -0800

    items(please check spelling if you can, aslo what else can I do)

---
+++ Global Game Jam 2019 Jan/Assets/Scripts/collectaableItems.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "items", menuName = "data/items", order = 1)]
+public class collectaableItems : ScriptableObject
+{
+    public string itemsName;
+    public float score;
+    public string discripition;
+    public Sprite itemPic;
+}
