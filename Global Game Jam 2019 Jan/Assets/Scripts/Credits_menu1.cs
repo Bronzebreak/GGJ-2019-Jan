0374604a538aa8f297f1f07e9ee00fbb4d8fb539
ReaganShor <Boondoggling247@gmail.com>
2019-01-26 15:48:10 -0800

    Creesdits menu added

---
+++ Global Game Jam 2019 Jan/Assets/Scripts/Credits_menu1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits_menu1 : MonoBehaviour
+{
+    //Variables
+    public string levelToLoad;
+
+    public void OnClickedMenu()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+	
+}
