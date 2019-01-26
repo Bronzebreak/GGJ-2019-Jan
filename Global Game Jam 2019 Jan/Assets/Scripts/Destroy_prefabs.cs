7f429deabbb276d30cb535f6aefa75c53e27c05d
ReaganShor <Boondoggling247@gmail.com>
2019-01-26 12:11:07 -0800

    Added player respawn, and onld tile destroy. Other areas updated

---
+++ Global Game Jam 2019 Jan/Assets/Scripts/Destroy_prefabs.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_prefabs : MonoBehaviour
+{
+    //Variables
+    public GameObject floor;
+    
+	void Start ()
+    {
+	
+	}
+	
+	
+	void Update ()
+    {
+    
+	}
+
+    private void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.gameObject.tag == "Old_floor")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
