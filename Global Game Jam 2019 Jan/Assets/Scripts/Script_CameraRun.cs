e0fd422342d5986fbbf9183c87b82adede6e6fc2
AndrewKomlyev <andrew.komlyev@gmail.com>
2019-01-26 12:22:00 -0800

    Camera Movement, and obstacle spawninig scripts

---
+++ Global Game Jam 2019 Jan/Assets/Scripts/Script_CameraRun.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_CameraRun : MonoBehaviour {
+    public Transform MainCamera;
+
+    // Use this for initialization
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(MainCamera.position.x + 0.1f, 0, -10);
+    }
+}
---
+++ Global Game Jam 2019 Jan/Assets/Scripts/Script_RandomObstacle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_RandomObstacle : MonoBehaviour
+{
+    public GameObject[] obstacle;
+    public float spawnMin = 1.0f;
+    public float spawnMax = 3.0f;
+    // Use this for initialization
+    void Start()
+    {
+        Spawn();
+    }
+    void Spawn()
+    {
+        Instantiate(obstacle[Random.Range(0, obstacle.GetLength(0))], transform.position, Quaternion.identity);
+        Invoke("Spawn", Random.Range(spawnMin, spawnMax));
+    }
+}
