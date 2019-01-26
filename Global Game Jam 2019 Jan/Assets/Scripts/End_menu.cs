53ba0df4ee06e333ba7e039aa052e0f47c18e5b9
ReaganShor <Boondoggling247@gmail.com>
2019-01-26 14:43:52 -0800

    Updated folders,  and new script added

---
+++ Global Game Jam 2019 Jan/Assets/Scripts/End_menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End_menu : MonoBehaviour
+{
+    //Variables
+    public string levelToLoad;
+    public string mainMenu;
+
+
+    public void OnClickedRestart()
+    {
+
+    }
+}
