1d3adb31a5e8452c1bb4e72a3a651cbd71490d2a
Derek Penman <wolf_lord@hotmail.com>
2019-01-26 14:03:30 -0800

    no message

---
+++ Global Game Jam 2019 Jan/Assets/Scripts/Collectibles.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour
+{
+    public string itemName;
+    public string descripition;
+    public Sprite sprite;
+}
