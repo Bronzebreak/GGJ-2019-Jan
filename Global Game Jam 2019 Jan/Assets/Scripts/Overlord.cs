d1b08901612b17697b3bdf557f6803dfcdc13ed7
Derek Penman <wolf_lord@hotmail.com>
2019-01-25 21:02:25 -0800

    Edited Overlord script.

--- Global Game Jam 2019 Jan/Assets/Scripts/Overlord.cs
+++ Global Game Jam 2019 Jan/Assets/Scripts/Overlord.cs
@@ -1,16 +1,92 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Overlord : MonoBehaviour {
+public class Overlord : MonoBehaviour
+{
+
+    //Calculated current angle of building in relation to map/background. If it hits ~45* or -45*, gameOver should occur.
+    public int angleRotation = 0;
+    public float timer;
+    public float secondTimer;
+    public string scene;
+    bool tiltingRight;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    //Once a frame...
+    private void Update()
+    {
+        // ...count up time.
+        secondTimer += Time.deltaTime;
+
+        // ...if ~1s of time has surpassed...
+        if (secondTimer >= 1)
+        {
+            // ...if tilting to the right...
+            if (tiltingRight == true)
+            {
+                // ...time remaining is equal to the difference between 45 and the current rotation.
+                timer = 45 - angleRotation;
+            }
+
+            // ...if tilting to the left...
+            if (tiltingRight == false)
+            {
+                // ...time remaining is equal to the difference between -45 and the current rotation.
+                timer = -45 + angleRotation;
+            }
+
+            // ...then print timer...
+            print (timer);
+
+            // ...then reset timer.
+            secondTimer = 0;
+        }
+
+        
+       
+
+        
+
+        
+
+    }
+
+    //Multiple times a frame...
+    void FixedUpdate()
+    {
+        // ...if Esc is pressed...
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // ...execute 'Quit' function.
+            QuitGame();
+        }
+
+        // ...if R is pressed...
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // ...execute 'RestartLevel' function.
+            RestartGame();
+        }
+    }
+
+    //When function is called...
+    public void RestartGame()
+    {
+        // ...load scene of publicly set name 'scene'.
+        SceneManager.LoadScene(scene);
+    }
+
+    //When function is called...
+    public void QuitGame()
+    {
+        // ...exit application.
+        Application.Quit();
+    }
 }
