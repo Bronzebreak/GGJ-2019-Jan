4210b249052528f3910c76f0cbb9f7bee715c2c9
Kayleigh Bezaire <kayleigh.bezaire@gmail.com>
2019-01-25 21:38:49 -0800

    objects

---
+++ Global Game Jam 2019 Jan/Assets/Scripts/MoveableObjects.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableObjects : MonoBehaviour {
+   /* int[,] objects = new int[16, 3]
+        {
+            {"Corgi plush","weight: 1 pounds","A plush toy from childhood, it's torn in a few places" },
+            {"Sports trophy", "weight: 4 pounds", "A 2nd place Judo trophy, the inscribetion is wrong though" },
+            {"Table", "weight: 15 pounds", "An old oak table, you can still see the scrach marks for years ago" },
+            {"Chair","weight: 14 pounds", "An leather reclining chair, there is a destint butt mark in the seat" },
+            {"China cabinet", "weight: 100 pounds", "A cabinet full of wedding china, why did you two pick this style anyway?"},
+            {"Photo", "weight: 0.5 pounds", " A photograph of a happy couple, a warm feel starts whenever you look at it"},
+            {"Dresser", "weight: 50 pounds", "A old oak dresser, it's full of old and tattered clothes" },
+            {"Computer desk", "weight: 30 pounds", "A computer desk covered in documents, you spent to much time here"},
+            {"TV", "weight: 10 pounds", " An old CRT TV, why do you keep falling asleep in front of this thing" },
+            {"Ring", "weight: 0.01 pounds", "A small engagement ring, you remember the look on her face when you showed it to her"},
+            {"Mirror", "weight: 5 pounds", "A broken mirror, you can't even remember what that fight was about" },
+            {"couch", "weight: 40 pounds", "A old worn couch, you still feel her warmth whenever you lay down"},
+            {"Wine bottle", "weight: 2 pounds", "A vinage wine bottle, you wish you had a chance to drink it with her" },
+            {"Painting", "weight: 10 pounds", "An strange painting, you have no idea why she liked it so much"},
+            {"Vase", "weight: 6 pounds", "A vase with a makeshift lid, you hope she's happy with where you put her" },
+            {"Pizza", "weight: 3 pounds", "A pineapple pizza, why did you get this you hate pineapple" }
+        };
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    */
+}
