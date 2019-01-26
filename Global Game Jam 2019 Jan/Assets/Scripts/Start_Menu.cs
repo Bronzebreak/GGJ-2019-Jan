1d5ead482aafe5d55122724eaea59bdade850644
ReaganShor <Boondoggling247@gmail.com>
2019-01-26 15:17:04 -0800

    Created  start menu and fist stages of the credits menu

---
+++ Global Game Jam 2019 Jan/Assets/Scripts/Start_Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start_Menu : MonoBehaviour
+{
+    //Varabiles
+    public string levelToLoad;
+    public string creditsMenu;
+
+    public void OnClickedStart()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+
+    public void OnClickedCredits()
+    {
+        SceneManager.LoadScene(creditsMenu);
+    }
+
+    public void OnClickedExit()
+    {
+        Application.Quit();
+    }
+
+
+}
