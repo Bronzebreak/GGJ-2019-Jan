7662334040eadb385d5248215a9bba5af1519f13
Derek Penman <wolf_lord@hotmail.com>
2019-01-26 16:56:02 -0800

    no message

--- Global Game Jam 2019 Jan/Assets/Scripts/Credits_menu1.cs
+++ Global Game Jam 2019 Jan/Assets/Scripts/Credits_menu1.cs
@@ -8,9 +8,33 @@ public class Credits_menu1 : MonoBehaviour
     //Variables
     public string levelToLoad;
 
-    public void OnClickedMenu()
+    //Once a frame...
+    private void Update()
+    {
+        // ...if R is pressed...
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // ...execute restarting function.
+            RestartGame();
+        }
+    }
+
+    public void LoadLevel()
     {
         SceneManager.LoadScene(levelToLoad);
     }
-	
+
+    //When function is called...
+    public void RestartGame()
+    {
+        // ...load scene of publicly set name 'scene'.
+        SceneManager.LoadScene("Menu");
+    }
+
+    //When function is called...
+    public void QuitGame()
+    {
+        // ...exit application.
+        Application.Quit();
+    }
 }
