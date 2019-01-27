3514cfc23fe3744b63b1e8e87303d50f4ab6c106
Derek Penman <wolf_lord@hotmail.com>
2019-01-27 13:19:34 -0800

    no message

--- Global Game Jam 2019 Jan/Assets/Scripts/Tile_Spawner.cs
+++ Global Game Jam 2019 Jan/Assets/Scripts/Tile_Spawner.cs
@@ -2,34 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour
+public class Tile_Spawner : MonoBehaviour
 {
     public GameObject[] spawnedItems;
     public float spawnDelay;
     public float delayTimer;
     public Player playerRef;
+    private bool canSpawn;
 
-    //Upon initialization...
-    private void Start()
+    //Multiple times a frame...
+    void FixedUpdate()
     {
-        // ...run Spawn function after the publicly set spawnDelay.
-        Invoke("Spawn", spawnDelay);
+        // ...if the player is NOT in the house and canSpawn is true...
+        if (playerRef.safeZone == false && canSpawn == true)
+        {
+            // ...begin Spawn function...
+            Spawn();
+
+            // ...and disallow further spawn calls.
+            canSpawn = false;
+        }
+
+        // ...if the player IS in the house...
+        else if (playerRef.safeZone == true)
+        {
+            // ...spawn can be called.
+            canSpawn = true;
+        }
     }
 
     //When function is called...
     void Spawn()
     {
-        // ...as long as player is not in the House...
+        // ...if the player is not in the house...
         if (playerRef.safeZone == false)
         {
             // ...create an item from the spawnedItems Array at spawner's location, with spawner's quaternion...
             Instantiate(spawnedItems[Random.Range(0, spawnedItems.GetLength(0))], transform.position, Quaternion.identity);
-            
+
             // ...and run the function again after the publicly set spawnDelay.
             Invoke("Spawn", spawnDelay);
         }
-
-        // ...if player is in the House, run the function again after the publicly set spawnDelay.
-        else Invoke("Spawn", spawnDelay);
     }
 }
