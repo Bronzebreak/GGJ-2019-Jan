1bc69042902d8568f5540728542a064b13419a22
ReaganShor <Boondoggling247@gmail.com>
2019-01-27 16:34:52 -0800

    no message

---
+++ Global Game Jam 2019 Jan/Assets/Scripts/Item_discription.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item_discription : MonoBehaviour
+{
+    public string currentItem;
+
+
+    public void OnMouseEnter(Collider obj)
+    {
+        if (obj.gameObject.tag == "collect")
+        {
+           gameObject.GetComponent<Collectibles>().descripition = currentItem;
+           
+        }
+    }
+    
+}
