2d22feda858b1cc6b0ddc690744f73be577f0b9a
AndrewKomlyev <andrew.komlyev@gmail.com>
2019-02-24 13:19:34 -0800

    Spawn

--- Global Game Jam 2019 Jan/Assets/Scripts/Spawner.cs
+++ Global Game Jam 2019 Jan/Assets/Scripts/Spawner.cs
@@ -4,35 +4,60 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public float spawnDelay;
-    public float delayTimer;
-    public Player playerRef;
+    //public float spawnDelay;
+    //public float delayTimer;
+    public GameObject itemRef;
+    int pTouched;
+    //public Player playerRef;
     public List<GameObject> collectiblesList;
+    Vector3 newPosition;
 
     //Upon initialization...
     private void Start()
     {
         // ...run Spawn function after the publicly set spawnDelay.
-        Invoke("Spawn", spawnDelay);
+        //Invoke("Spawn", spawnDelay);
+        newPosition = itemRef.transform.position;
     }
-
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            if(pTouched == 0)
+            {
+                Spawn();
+                print("spawn");   
+            }
+        }
+    }
+        void OnTriggerexit2D(Collider2D col)
+    {
+        if(pTouched == 1)
+        {
+            pTouched=1;
+        }
+    }
     //When function is called...
     void Spawn()
     {
         // ...as long as player is not in the House...
-        if (playerRef.safeZone == false)
-        {
+        //if (playerRef.safeZone == false)
+        //{
             GameObject Item;
+            newPosition.x +=25.0f;
+            newPosition.y = 0;
 
             // ...create an item from the spawnedItems Array at spawner's location, with spawner's quaternion...
-            Item = Instantiate(collectiblesList[Random.Range(0, collectiblesList.Count)], transform.position, Quaternion.identity);
+            Item = Instantiate(collectiblesList[Random.Range(0, collectiblesList.Count)], newPosition, Quaternion.identity);
             Item.gameObject.SetActive(true);
+            newPosition = itemRef.transform.position;
+            
 
             // ...and run the function again after the publicly set spawnDelay.
-            Invoke("Spawn", spawnDelay);
-        }
+            //Invoke("Spawn", spawnDelay);
+        //}
 
         // ...if player is in the House, run the function again after the publicly set spawnDelay.
-        else Invoke("Spawn", spawnDelay);
+        //else Invoke("Spawn", spawnDelay);
     }
 }
---
+++ Global Game Jam 2019 Jan/Assets/Scripts/Trigger_Spawner.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_Spawner : MonoBehaviour 
+{
+	// reference to the trigger prefab
+	public GameObject spawnedItems;
+	// reference to the object that spawns 
+
+    public GameObject itemsRefTest;
+    //public float spawnDelay;S
+    //public float delayTimer;
+    public Player playerRef;
+    private bool canSpawn;
+    //float xPosition  = 0;
+	Vector3 newPosition;
+
+	// Use this for initialization
+    void start()
+    {
+        newPosition = itemsRefTest.transform.position;
+    }
+	
+	// Update is called once per frame
+    void FixedUpdate()
+    {
+        // ...if the player is NOT in the house and canSpawn is true...
+        if (playerRef.safeZone == false && canSpawn == true)
+        {
+            // ...begin Spawn function...
+            Spawn();
+
+            // ...and disallow further spawn calls.
+            canSpawn = false;
+        }
+
+        // ...if the player IS in the house...
+        else if (playerRef.safeZone == true)
+        {
+            // ...spawn can be called.
+            canSpawn = true;
+        }
+        newPosition = itemsRefTest.transform.position;
+    }
+	 void Spawn()
+    {
+        newPosition.x += 22.5f;
+        newPosition.y = -4.517f;
+        // ...if the player is not in the house...
+        if (playerRef.safeZone == false)
+        {
+            // ...create an item from the spawnedItems Array at spawner's location, with spawner's quaternion...
+            //Instantiate(spawnedItems[Random.Range(0, spawnedItems.GetLength(0))], newPosition, Quaternion.identity);
+
+            // ...and run the function again after the publicly set spawnDelay.ы
+            //Invoke("Spawn", spawnDelay);
+            for (int i = 0; i <5; i++) 
+            {         
+                Instantiate(spawnedItems, newPosition, Quaternion.identity);
+                newPosition.x +=90.0f;
+                //transform.position = newPosition;
+                //print(newPosition);
+            }
+        }
+    }
+}
