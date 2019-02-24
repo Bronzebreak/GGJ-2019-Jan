b9c92bc563b765ecaf41dd246344880ab75655be
Reagan Shorrock <boondoggling247@gmail.com>
2019-02-24 11:57:07 -0800

    (almost) finished animations

--- Global Game Jam 2019 Jan/Assets/Scripts/Player_movement_Animation.cs
+++ Global Game Jam 2019 Jan/Assets/Scripts/Player_movement_Animation.cs
@@ -1,19 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Player_movement_Animation : MonoBehaviour
 {
+    //Variables
 
-	// Use this for initialization
-	void Start ()
+    public Animator ani;
+    public Player refrence;
+
+    public bool canJump;
+    public bool isRunning;
+
+
+
+    // Sees if the player if jumping, if true will play the jumping animation.
+    public void jump_cheak()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        if (canJump == true)
+        {
+            ani.SetBool("Bool_isJumping", true);
+        }
+        else if (canJump == false)
+        {
+            ani.SetBool("Bool_isJumping", false);
+        }
+    }
+
+
+    // Sees if the player if running, if true will play the running animation.
+    public void run_cheak()
     {
-		
-	}
+        if (isRunning == true)
+        {
+            ani.SetBool("Bool_isRunning", true);
+        }
+        else if (isRunning == false)
+        {
+            ani.SetBool("Bool_isRunning", false);
+        }
+    }
 }
