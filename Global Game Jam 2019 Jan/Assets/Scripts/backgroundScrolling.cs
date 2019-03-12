3e2e01e2dd55a641c1d77a57ca3d770d8e6eb0a1
Reagan Shorrock <boondoggling247@gmail.com>
2019-03-11 20:31:20 -0700

    moving background

---
+++ Global Game Jam 2019 Jan/Assets/Scripts/backgroundScrolling.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundScrolling : MonoBehaviour
+{
+    Material mat;
+    Vector2 bgOffSet;
+
+    public float xVel, yVel;
+
+    // Use this for initialization
+
+
+    private void Awake()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        bgOffSet = new Vector2(xVel, yVel);
+        mat.mainTextureOffset += bgOffSet * Time.deltaTime;
+	}
+}
